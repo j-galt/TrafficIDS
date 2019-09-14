c6e85d63772a013dc1e6ad44722eec03692b186e
j-galt <illya.pivt@gmail.com>
2019-09-14 17:47:41 +0300

    initial commit for proj

---
+++ TrafficIDS/Domain/MeterData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrafficIDS.Domain
+{
+	public class MeterData
+	{
+		[LoadColumn(0)]
+		public string Name { get; set; }
+		[LoadColumn(1)]
+		public DateTime Time { get; set; }
+		[LoadColumn(2)]
+		public float Value { get; set; }
+	}
+}
